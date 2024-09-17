83fbad7e3f3e75fc68607ece37a249cac9ee07f7
anaschlz <analu.sschulz@gmail.com>
2024-09-17 18:51:54 -0300

    lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert} from "react-native";
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
 
 export default function Home() {
+    const participants = ['Ana', 'Luisa', 'Yasmin', 'Victor', 'Fernando', 'Noah', 'Kauan', 'Arthur', 'João', 'Lucas']
 
     function handleParticipantAdd() {
-        console.log("Participant added");
+        if(participants.includes("Ana")){
+            return Alert.alert("Participante já existe", "Já existe um participante na lista com esse nome.");
+        }
+
     }
 
     function handleParticipantRemove(name: string) {
-        console.log(`Participant ${name} removed`);
+        Alert.alert("Remover participante", "Deseja remover participante da lista?", [
+            {
+                text: 'Sim',
+                onPress: () => Alert.alert('Deletado!')
+            },
+            {
+                text: 'Não',
+                style: 'cancel'
+            }
+        ]);
     }
 
 
@@ -37,7 +50,28 @@ export default function Home() {
                 </TouchableOpacity>
             </View>
 
-        <Participant name="Ana" onRemove={() => handleParticipantRemove("Ana")}/>
+            <FlatList
+                data={participants}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Participant
+                        key={item}
+                        name={item}
+                        onRemove={() => handleParticipantRemove(item)}
+                        />
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => (
+                    <Text style={styles.listEmptyText}>
+                        Nenhum participante encontrado! Adicione participantes a sua lista de convidados!
+                    </Text>
+                )}
+                
+
+            />
+
+
+
         </View>
     )
 }
